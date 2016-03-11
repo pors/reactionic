34a77695f6c77e267c054ead7bc121c966abd9dc
Mark Pors <mark@pors.net>
2016-03-11 09:52:57 +0100

    fix platform switcher

--- js/main.js
+++ js/main.js
@@ -10,7 +10,20 @@ $(window).scroll(function() {
 // On mobile, hide the avatar when expanding the navbar menu
 $('#main-navbar').on('show.bs.collapse', function () {
   $(".navbar").addClass("top-nav-expanded");
-})
+});
 $('#main-navbar').on('hidden.bs.collapse', function () {
   $(".navbar").removeClass("top-nav-expanded");
-})
+});
+
+// Platform switcher, code from https://github.com/meteoric/meteoric.github.io/blob/master/index.html
+var demoUrl = 'https://app.pors.net';
+var $iframe = $('iframe.device-content');
+$('[data-platform]').bind('click', function (event) {
+  event.preventDefault();
+  $('[data-platform]').removeClass('selected');
+  $(this).addClass('selected');
+  var $iframe = $('iframe.device-content');
+  var platform = $(this).data('platform');
+  $iframe.attr('src', demoUrl + '?platformOverride=' + platform);
+});
+$('[data-platform=iOS]').click();
